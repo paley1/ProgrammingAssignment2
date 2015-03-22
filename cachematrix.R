4c482438f8ed63b1d1b73fa04bc0d78669e9b725
paley1 <paley1@users.noreply.github.com>
2015-03-22 02:40:21 -0700

    update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## this function works creating a list of 4 member functions(set,get,setmatrix,getmatrix)
+## <<- is used to change variable from outside environment to internal
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setmatrix<-function(solve) m<<- solve
+  getmatrix<-function() m
+  list(set=set, get=get,
+       setmatrix=setmatrix,
+       getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## solves for the inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getmatrix()
+  if(!is.null(m)){ ##message for getting cache data if inversion successful
+    message("getting cache")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m ##returns matrix
 }
